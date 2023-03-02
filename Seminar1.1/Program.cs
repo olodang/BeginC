3df7f384b64f23a5da530659516d95823feb5231
Morozova Natasha <olodang@gmail.com>
2023-03-02 14:58:34 +0300

    Days+Switch

--- Seminar1.1/Program.cs
+++ Seminar1.1/Program.cs
@@ -2,11 +2,7 @@
 int numberB = Convert.ToInt32(Console.ReadLine());
 int squareB = numberB * numberB;
 if(squareB == numberA)
-    {
-        Console.WriteLine($"yes, {numberA} is a square for {numberB}");
-    }
+     Console.WriteLine($"yes, {numberA} is a square for {numberB}");
 else 
-    {
         Console.WriteLine($"{numberA} isn`t square for {numberB}");
-    }
 
--- Seminar1.3/Program.cs
+++ Seminar1.3/Program.cs
@@ -1,13 +1,27 @@
 ﻿int number = Convert.ToInt32(Console.ReadLine());
-if(number==3)
+//if(number==3)
+//{
+//    Console.WriteLine("СРЕДА");
+//}
+//else if(number==5)
+//{
+  //  Console.WriteLine("ПЯТНИЦА");
+//}
+//else 
+//{
+ //   Console.WriteLine($"{number}");
+//}
+
+switch(number)
 {
-    Console.WriteLine("СРЕДА");
-}
-else if(number==5)
-{
-    Console.WriteLine("ПЯТНИЦА");
-}
-else 
-{
-    Console.WriteLine($"{number}");
+    default: Console.WriteLine($"{number}");
+    break;
+
+    case 3:
+       Console.WriteLine("СРЕДА");
+       break;
+    case 5:
+        Console.WriteLine("ПЯТНИЦА");
+        break;
+    
 }
