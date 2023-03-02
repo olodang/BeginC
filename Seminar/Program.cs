c7ebe37a36ff7fa5e80da3a3508b52d0b5a2e0e7
Morozova Natasha <olodang@gmail.com>
2023-03-02 12:43:03 +0300

    number*number

---
+++ Seminar/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+string hello = "hello";
+
+
+Console.WriteLine("Your result is: " + result);
+Console.WriteLine("Your {1} result is: {0}", result, hello);
+Console.WriteLine($"Your result is {number * number}");
+
