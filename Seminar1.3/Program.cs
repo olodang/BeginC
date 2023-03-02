d1ab32c14848b590ab98c03e28ce9fab20c60248
Morozova Natasha <olodang@gmail.com>
2023-03-02 14:21:38 +0300

    DaysOfWeek

---
+++ Seminar1.3/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+if(number==3)
+{
+    Console.WriteLine("СРЕДА");
+}
+else if(number==5)
+{
+    Console.WriteLine("ПЯТНИЦА");
+}
+else 
+{
+    Console.WriteLine($"{number}");
+}
