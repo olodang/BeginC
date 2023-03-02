1c5cc8e41474adf5323523441d7f5a2d48a4294c
Morozova Natasha <olodang@gmail.com>
2023-03-02 13:55:34 +0300

    square

---
+++ Seminar1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int squareB = numberB * numberB;
+if(squareB == numberA)
+    {
+        Console.WriteLine($"yes, {numberA} is a square for {numberB}");
+    }
+else 
+    {
+        Console.WriteLine($"{numberA} isn`t square for {numberB}");
+    }
+
