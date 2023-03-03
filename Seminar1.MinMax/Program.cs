5b53a28be813e1399a41001406bfb5807301acc2
Morozova Natasha <olodang@gmail.com>
2023-03-03 12:54:05 +0300

    FromMinToMax

---
+++ Seminar1.MinMax/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter number");
+int MaxNumber = Convert.ToInt32(Console.ReadLine());
+int MinNumber = -MaxNumber;
+Console.WriteLine("Вывод целых чисел");
+while(MinNumber<=MaxNumber)
+{
+    Console.Write(" "+MinNumber);
+    MinNumber +=1;
+}
