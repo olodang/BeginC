008a2478f2f3e0d6e969de07cfe5be1ace74b5f4
Morozova Natasha <olodang@gmail.com>
2023-03-03 12:20:51 +0300

    lastnum

--- Seminar1.4/Program.cs
+++ Seminar1.4/Program.cs
@@ -1,8 +1,12 @@
-﻿Console.WriteLine("Введите число");
-int MaxNumber = Convert.ToInt32(Console.ReadLine());
-int MinNumber = MaxNumber * -1;
-while(MinNumber<=MaxNumber)
-   {
-    Console.WriteLine(MinNumber);
-    MinNumber+=1;
-   }
+﻿Console.WriteLine("Enter a three-digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastnum = 0;
+if(number>=100 && number<1000)
+{
+    lastnum = number%10;
+    Console.WriteLine(lastnum);
+}
+else
+{
+    Console.WriteLine("Enter a three-digit number");
+}
