8db10019a46b2f21919a274d5f19ce1dd849bfa4
Morozova Natasha <olodang@gmail.com>
2023-03-09 16:36:17 +0300

    MethodArray

---
+++ MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {1,5,8,9,4,3,5};
+int n = array.Length;
+int find = 5;
+int index = 0;
+while (index<n)
+{
+    if(array[index]==find)
+        {Console.WriteLine(index);
+    break;
+        }
+    index++;
+
+}
