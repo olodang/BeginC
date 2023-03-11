72bb025e2116235d114305b5a9517e6e0d79140c
Morozova Natasha <olodang@gmail.com>
2023-03-11 19:11:44 +0300

    Replace

---
+++ Lection3.ExamplesOfTasks/Program.cs
@@ -0,0 +1,22 @@
+﻿// // Функции в прораммировании. 
+// Дан текст. В тексте нужно все пробелы заменить черточками, маленькие буквы "к" заменить "К",
+// а большие "С" заменить маленькими "с"
+string text = "Характер девушки-Близнецы многогранен";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    
+    int length = text.Length;
+    
+    for (int i = 0; i<length; i++)
+    {   
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result+ $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+
---
+++ Lection3.WHILEandFOR/Program.cs
@@ -0,0 +1,10 @@
+﻿string Method4(int count, string c)
+{
+    string result = string.Empty;
+    for(int i=0;i<count;i++)
+ 
+    {result= result+c;}
+    return result;
+}
+string res = Method4(10, "qwert");
+Console.WriteLine(res);
